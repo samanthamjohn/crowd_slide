4c07f1f28ff942716a291d3278124ad5f8810846
Ben Moss <ben@mossity.com>
2011-08-20 19:25:03 -0400

    head 422 if the record doesn't save

--- app/controllers/sms_controller.rb
+++ app/controllers/sms_controller.rb
@@ -8,6 +8,8 @@ class SmsController < ApplicationController
     sms = Sms.new(body: params["Body"], phone_number: params["From"])
     if sms.save
       head 200
+    else
+      head 422
     end
   end
 end
