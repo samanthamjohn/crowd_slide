d35907e3af3f10978ccfd751baa7c19c7e109c4f
Benjamin Moss <mossity@Benjamins-MacBook.local>
2011-08-20 13:39:21 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Twilio500px::Application.config.secret_token = '594fa17a0416b0acf435290437277fd818471ffad18086e2c6b1b9be64d49e65089c2ac449ceeabe9efa7e535d0f91c875f856b1d60002228a00d6a6b3650621'
