239627623b4e004d3054eb0960cdbeafdded99a2
Ben Moss & Samantha John <pair+ben+samantha@example.com>
2011-08-21 02:31:09 +0000

    Add keywords and better instructions to the page.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,13 +2,13 @@ class HomeController < ApplicationController
   def index
     sms = Sms.last
     @photos = sms.photos
+    @keyword = sms.body
     sms.update_attribute(:displayed, true)
   end
 
   def latest
     sms = Sms.undisplayed.last || Sms.first(:order => "random()")
-    photos = sms.photos
     sms.update_attribute(:displayed, true)
-    render :partial => "photos", :locals => { :photos => photos }
+    render :partial => "photos", :locals => { :photos => sms.photos, :keyword => sms.body }
   end
 end
